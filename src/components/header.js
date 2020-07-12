eb8a968c2250872bb472f2effcf00b83eb569fea
bs645 <bs645@cornell.edu>
2020-07-12 16:06:37 -0400

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,53 @@
+import React, { useState } from "react"
+
+import classnames from "classnames"
 import { Link } from "gatsby"
-import React from "react"
 import styles from "../styles/components/header.module.scss"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const cx = classnames.bind(styles)
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function toggleIsOpen() {
+    setIsOpen(value => !value)
+  }
+
+  return (
+    <header class={styles.header}>
+      <div class={styles.container}>
+        <div class={styles.navbarHeader}>
+          <Link className={styles.title} to="/">
+            Brandon Stein
+          </Link>
+          <button class={styles.toggle} onClick={toggleIsOpen}>
+            <span class={styles.iconBar}></span>
+            <span class={styles.iconBar}></span>
+            <span class={styles.iconBar}></span>
+          </button>
+        </div>
+        <div class={cx(styles.navbarContent, { [styles.open]: isOpen })}>
+          <ul class={styles.nav}>
+            <li>
+              <Link to="/projects/voltorbflip">Voltorb Flip</Link>
+            </li>
+            <li>
+              <a href="https://github.com/steiner26">GitHub</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/bstein26/">LinkedIn</a>
+            </li>
+            <li>
+              <a href="/assets/resume.pdf">Resume</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  )
+}
 
 export default Header
